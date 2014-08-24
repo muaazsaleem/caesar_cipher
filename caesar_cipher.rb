30c911385cbecb58495e11c533280939ecd7df33
Muaaz Saleem <muaazsaleem@outlook.com>
2014-08-24 09:43:20 +0500

    Changed Implementation to include Space

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,16 +3,18 @@
 def caesar_cipher string_param, shift_param
 	
 	alphabet = ('a'..'z').to_a
-	letters = string_param.chomp.downcase.scan(/\w/)
+	letters = string_param.strip.downcase.scan(/./)
 	
 	letters.each.with_index do |v,i|
-		key = alphabet.index(v) + shift_param
+		if v==" "
+			letters[i] = " "
+			next
+		end	
+		key = alphabet.index(v) + shift_param 
 		key -= 26 if key>25 
-		
 		letters[i] = alphabet[key]
 	end
 	letters.join
 end
 	
-
-p caesar_cipher("i love you  ", 5)
+caesar_cipher("Pcka Pocka nje  ", 5)
